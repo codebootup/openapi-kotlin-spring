1049e306e6a52159d5329a24a2e899b41d58b582
Lee Cooper <lee@codebootup.com>
2023-04-14 06:48:32 +0800

    Initial

---
+++ src/test/kotlin/com/codebootup/codegenerator/GenerateOpenAPISpringKotlinCodeTest.kt
@@ -0,0 +1,10 @@
+package com.codebootup.codegenerator
+
+import org.junit.jupiter.api.Test
+
+class GenerateOpenAPISpringKotlinCodeTest {
+    @Test
+    fun test(){
+        GenerateOpenAPISpringKotlinCode()
+    }
+}
