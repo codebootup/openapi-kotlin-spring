61103aa1929bfb97ebffdb6c90d93c2a868a976a
codebootuphong <hong@codebootup.com>
2023-04-17 19:41:10 +0800

    #1 - added method to get vehicle by id

--- test/openapi-kotlin-spring-codegen-expected/src/main/kotlin/com/codebootup/vehicle/VehicleAPI.kt
+++ test/openapi-kotlin-spring-codegen-expected/src/main/kotlin/com/codebootup/vehicle/VehicleAPI.kt
@@ -1,6 +1,7 @@
 package com.codebootup.vehicle
 
 import com.codebootup.vehicle.model.Vehicle
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -14,6 +15,13 @@ interface VehicleAPI {
         produces = ["application/json"]
     )
     fun getVehicles(): List<Vehicle>
+
+    @RequestMapping(
+        method = [RequestMethod.GET],
+        value = ["/vehicles/{id}"],
+        produces = ["application/json"]
+    )
+    fun getVehicleById(@PathVariable("id") id: Int): Vehicle
 
     @RequestMapping(
         method = [RequestMethod.POST],
