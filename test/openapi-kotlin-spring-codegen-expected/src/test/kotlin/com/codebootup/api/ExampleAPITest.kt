ad00af0979f904c94bab30756896d4aca95c7d60
codebootuphong <hong@codebootup.com>
2023-05-02 18:42:28 +0800

    #21 - added POST tests

--- test/openapi-kotlin-spring-codegen-expected/src/test/kotlin/com/codebootup/api/ExampleAPITest.kt
+++ test/openapi-kotlin-spring-codegen-expected/src/test/kotlin/com/codebootup/api/ExampleAPITest.kt
@@ -1,14 +1,21 @@
 package com.codebootup.api
 
+import com.codebootup.api.model.ExampleDataClass
 import com.codebootup.api.model.ExampleDataInterface
+import com.codebootup.api.model.ExampleDataSubClass1
+import com.codebootup.api.model.ExamplePrimitiveClass
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ExampleAPITest(
@@ -26,7 +33,7 @@ class ExampleAPITest(
     }
 
     @Test
-    fun `Can get list of examples`() {
+    fun `Can get list of examples with auth`() {
         val headers = HttpHeaders()
         headers.setBasicAuth(user, password)
         val entity = HttpEntity<Any>(headers)
@@ -36,5 +43,58 @@ class ExampleAPITest(
             method = HttpMethod.GET,
             requestEntity = entity
         )
+    }
+
+    @Test
+    fun `Can post an example with auth`() {
+        val headers = HttpHeaders()
+        headers.setBasicAuth(user, password)
+        val entity = HttpEntity<Any>(
+            ExampleDataSubClass1(
+                location = ExamplePrimitiveClass("example primitive"),
+                exampleBoolean = true,
+                exampleDate = LocalDate.MAX,
+                exampleDataClass = ExampleDataClass(exampleString = "example string"),
+                exampleString = "example string",
+                exampleInteger = 3,
+                exampleDateTime = LocalDateTime.MAX,
+                exampleInteger64 = 64L,
+                exampleNumber = 3,
+                id = 3
+            ), headers
+        )
+
+        RestTemplate().exchange<List<ExampleDataInterface>>(
+            url = "http://localhost:8080/api/v1/examples",
+            method = HttpMethod.POST,
+            requestEntity = entity
+        )
+    }
+
+    @Test
+    fun `Throws 401 when trying to post an example`() {
+        val headers = HttpHeaders()
+        val entity = HttpEntity<Any>(
+            ExampleDataSubClass1(
+                location = ExamplePrimitiveClass("example primitive"),
+                exampleBoolean = true,
+                exampleDate = LocalDate.MAX,
+                exampleDataClass = ExampleDataClass(exampleString = "example string"),
+                exampleString = "example string",
+                exampleInteger = 3,
+                exampleDateTime = LocalDateTime.MAX,
+                exampleInteger64 = 64L,
+                exampleNumber = 3,
+                id = 3
+            ), headers
+        )
+
+        assertThatThrownBy {
+            RestTemplate().exchange<List<ExampleDataInterface>>(
+                url = "http://localhost:8080/api/v1/examples",
+                method = HttpMethod.POST,
+                requestEntity = entity
+            )
+        }.isInstanceOf(HttpClientErrorException.Unauthorized::class.java)
     }
 }
